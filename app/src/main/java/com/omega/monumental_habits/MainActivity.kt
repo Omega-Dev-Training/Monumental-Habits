7e0a70b6fdb07b979a67ad43fa24b68f9ee58c6a
KsenyaBol <ks.bol.vr@gmail.com>
2022-08-30 17:18:03 +0300

    finished

--- app/src/main/java/com/omega/monumental_habits/MainActivity.kt
+++ app/src/main/java/com/omega/monumental_habits/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.omega.monumental_habits
 
-import android.content.res.Resources
 import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
-import androidx.appcompat.view.menu.MenuView
+import androidx.annotation.RequiresApi
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tracking_habits)
@@ -20,12 +18,10 @@ class MainActivity : AppCompatActivity() {
         val nav_view: BottomNavigationView = findViewById(R.id.navigation)
         nav_view.itemIconTintList = null
 
-//        val menu_view: MenuItem= findViewById(R.id.avatar_menu)
-//        menu_view.icon = R.id.avatar_menu
-
         window.statusBarColor = Color.TRANSPARENT
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 }
