a1f34fc4662c8e9747109b8bbc320d04843cdd17
KsenyaBol <ks.bol.vr@gmail.com>
2022-08-30 12:20:28 +0300

    navigation_finished, selector

--- app/src/main/java/com/omega/monumental_habits/MainActivity.kt
+++ app/src/main/java/com/omega/monumental_habits/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.omega.monumental_habits
 
+import android.content.res.Resources
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
+import androidx.appcompat.view.menu.MenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tracking_habits)
+
+        val nav_view: BottomNavigationView = findViewById(R.id.navigation)
+        nav_view.itemIconTintList = null
+
+//        val menu_view: MenuItem= findViewById(R.id.avatar_menu)
+//        menu_view.icon = R.id.avatar_menu
 
         window.statusBarColor = Color.TRANSPARENT
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
