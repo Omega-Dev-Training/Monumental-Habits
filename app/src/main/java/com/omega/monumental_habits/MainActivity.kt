53634179016dc6fde58e8909c7a817323cb2ce6d
maximlozhechnikov <98010383+maximlozhechnikov@users.noreply.github.com>
2022-08-31 17:41:40 +0300

    Don't trouble trouble until trouble troubles you

--- app/src/main/java/com/omega/monumental_habits/MainActivity.kt
+++ app/src/main/java/com/omega/monumental_habits/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.omega.monumental_habits
 
 import android.graphics.Color
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.AdapterView
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tracking_habits)
 
         val nav_view: BottomNavigationView = findViewById(R.id.navigation)
         nav_view.itemIconTintList = null
+
+        val time = resources.getStringArray(R.array.Time)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
 
         window.statusBarColor = Color.TRANSPARENT
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
